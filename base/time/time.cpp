8d1e51e6547d77e85da82fa7d35f81dd25a5a15b
priest334 <priest334@126.com>
2016-12-23 15:36:08 +0800

    clock_gettime function

--- base/time/time.cpp
+++ base/time/time.cpp
@@ -44,7 +44,7 @@ namespace hlp {
 #else
 			struct timespec ts;
 			clock_gettime(CLOCK_MONOTONIC, &ts);
-			return ts.tv_sec * 1000 + ts.tv_usec / 1000000;
+			return ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
 #endif
 		}
 
